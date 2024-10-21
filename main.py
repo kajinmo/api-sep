3aa32acea655a2cae9b58bba4ec59ca140842cba
kajinmo <kajin.mo@hotmail.com>
2024-10-20 22:58:57 -0300

    adicionei o novo endpoint produtos

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 ﻿from fastapi import FastAPI
+from typing import List, Dict
+
 
 app = FastAPI()
+
+
+produtos: List[Dict[str, any]] = [
+    {
+            "id": 1,
+            "nome": "Smartphone",
+            "descricao": "Um telefone que é inteligente",
+            "preco": 1500.0,
+            "disponivel": True, 
+    },
+    {
+            "id": 2,
+            "nome": "Notebook",
+            "descricao": "Um computador que é móvel",
+            "preco": 3500.0,
+            "disponivel": False, 
+    },
+    {
+            "id": 3,
+            "nome": "Tablet",
+            "descricao": "Um dispositivo móvel",
+            "preco": 2500.0,
+            "disponivel": True, 
+    },
+]
+
 
 # definir a rota
 @app.get("/") # recebe requisições GET
 def ola_mundo(): #response
         return {'Olá': 'Mundo'}
 
+# definir a rota que puxa produto
+@app.get("/produtos")
+def listar_produtos():
+        return produtos
 
